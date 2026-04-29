39b4d0f02aa42d697f81e8331eaaccc88592104c
marce <marce@PC-ZetgameFox>
2026-04-29 13:22:06 +0200

    He añadido un syso diciendo que el 4 no es primo

--- src/NumeroPrimo/Main.java
+++ src/NumeroPrimo/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		System.out.println("Buenos dias Ceinmark, soy Marta Gomez");
 		System.out.println("Introduce un numero para averiguar si es primo o no");
 		ent.nextLine();
+		System.out.println("El numero 4 no es primo. Los divisores del 4 son 1, 2 y 4.");
 	}
 
 }
