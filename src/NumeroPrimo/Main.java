1e89e71e105ce3794e5bf55de870ed525b6f3a55
marce <marce@PC-ZetgameFox>
2026-04-29 13:15:26 +0200

    Proyecto de numero Primo

---
+++ src/NumeroPrimo/Main.java
@@ -0,0 +1,15 @@
+package NumeroPrimo;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+
+		System.out.println("Buenos dias Ceinmark, soy Marta Gomez");
+		System.out.println("Introduce un numero para averiguar si es primo o no");
+		ent.nextLine();
+	}
+
+}
